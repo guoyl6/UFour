c8ef9baadd819ce35bc4892591ea8c30b90a3921
guoyl6 <845173739@qq.com>
2016-10-09 15:03:47 +0800

    Update E.js
    repair

--- RES/js/E.js
+++ RES/js/E.js
@@ -22,23 +22,23 @@ E.prototype.beforeStart = function(func) {
 }
 
 E.prototype.start = function(func) {
-	return this.addFunc(this.start, func);
+	return this.addFunc(this.startExec, func);
 }
 
 E.prototype.end = function(func) {
-	return this.addFunc(this.end, func);
+	return this.addFunc(this.endExec, func);
 }
 
 E.prototype.exec = function(data) {
-	var deferred = jQuery.Deferred();
+	var deferred = jQuery.Deferred(), t = deferred;
 	[].concat(this.beforeStartExec)
 	  .concat(this.startExec)
 	  .concat(this.endExec)
 	  .forEach(function(func) {
-	  	deferred.then(func);
+	  	t = t.then(func);
 	  })
 	setTimeout(function() {
 		deferred.resolve(data);
 	});
-	return deferred.promise();
+	return t;
 }
