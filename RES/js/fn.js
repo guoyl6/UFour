f266048520d9e179caf67173ac259e86def1ede4
guoyl6 <845173739@qq.com>
2016-10-17 16:42:15 +0800

    Update fn.js

--- RES/js/fn.js
+++ RES/js/fn.js
@@ -1,35 +1,44 @@
 /*function maker*/
 
-(function() {
+(function () {
 	var slice = Array.prototype.slice;
 	var fp = Function.prototype;
-	var apply = function(func, args, ctx) {
+	var apply = function (func, args, ctx) {
 		// console.log(func, args, ctx);
 		return func.apply(getContext(this, ctx), args);
 	}
-	var getContext = function(defaultCtx, ctx) {
+	var getContext = function (defaultCtx, ctx) {
 		if (typeof ctx === "undefined") {
 			return defaultCtx;
 		} else {
 			return ctx;
 		}
 	}
-	fp.addSelf = function(ctx) {
-		var func = this;
-		return function() {
-			// debugger;
-			var args = slice.apply(arguments);
+	fp.addSelf = function (ctx) {
+		return function (func, args) {
 			args.unshift(this);
 			return apply.call(this, func, args, ctx);
-		}
+		}.withArrayLikeArguments(this);
 	}
-	fp.check = function(fn, ctx) {
-		var func = this;
-		return function() {
-			var args = slice.apply(arguments);
+	fp.check = function (fn, ctx) {
+		return function (func, args) {
 			if (apply.call(this, fn, args, ctx)) {
 				return apply.call(this, func, args, ctx);
 			}
+		}.withArrayLikeArguments(this);
+	}
+	fp.returnSelf = function (ctx) {
+		return function (func, args) {
+			apply.call(this, func, args, ctx);
+			return this;
+		}.withArrayLikeArguments(this);
+	}
+	fp.withArrayLikeArguments = function () {
+		var func = this, extras = slice.apply(arguments);
+		return function () {
+			var args = slice.apply(arguments);
+			extras.push(args);
+			return apply.call(this, func, extras, undefined);
 		}
 	}
 })();
