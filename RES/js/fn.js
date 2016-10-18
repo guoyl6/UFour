ea18829bfe83441fc82a1fdc5a635ec39dd8b485
guoyl6 <845173739@qq.com>
2016-10-18 13:18:14 +0800

    Update fn.js

--- RES/js/fn.js
+++ RES/js/fn.js
@@ -41,4 +41,87 @@
 			return apply.call(this, func, args, undefined);
 		}
 	}
+
+	var _throttle = function (fn, opt) {
+		var isIdle = true, toBeIdle = null, next = null, lastExecTime = null;
+		var immediately = opt.immediately, delay = opt.delay || 0,
+			debounce = opt.debounce || false, tail = opt.tail || false;
+		if (!(immediately in opt)) {
+			opt.immediately = true;
+		}
+		nextOn = function () {
+			var diff = lastExecTime || delay, args = arguments;
+			if (lastExecTime) {
+				var now = Date.now();
+				diff = now - (!debounce ? lastExecTime : now) - delay;
+				diff = diff < 0 ? -diff : 0;
+			}
+			if (tail || !diff || debounce) {
+				// console.log(diff);
+				next = setTimeout(function () {
+					exec.apply(this, args);
+					next = null;
+				}.bind(this), diff);
+			}
+				
+		},
+		nextOff = function () {
+			clearTimeout(next);
+			next = null;
+		},
+		exec = function () {
+			fn.apply(this, arguments);
+			lastExecTime = Date.now();
+		},
+		busy = function () {
+			isIdle = false;
+			clearTimeout(toBeIdle);
+			toBeIdle = setTimeout(function () {
+				isIdle = true;
+				lastExecTime = null;
+				toBeIdle = null;
+			}, delay);
+		};
+		return function () {
+			if (lastExecTime === null) {
+				lastExecTime = Date.now();
+			}
+			if (isIdle) {
+				if (immediately) {
+					exec.apply(this, arguments);
+				} else {
+					nextOn.apply(this, arguments);
+				}
+			} else {
+				if (debounce) {
+					if (!immediately) {
+						nextOff();
+						nextOn.apply(this, arguments);
+					}
+				} else if (next === null) {
+					nextOn.apply(this, arguments);
+				}
+			}
+			busy();
+		};
+	}
+
+	fp.debounce = function (delay, immediately) {
+		return _throttle(this, {
+			immediately: immediately,
+			delay: delay,
+			debounce: true
+		})
+	}
+
+
+	fp.throttle = function (delay, immediately, tail) {
+		return _throttle(this, {
+			delay: delay,
+			immediately: immediately,
+			debounce: false,
+			tail: tail
+		});
+	}
+
 })();
